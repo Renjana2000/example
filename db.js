67d9d617b167b7ade93db5425cc4e894a9aeef09
Renjana2000 <renjanasuresh464@gmail.com>
2025-08-13 11:46:34 +0530

    Initial commit - backend code

--- db.js
+++ db.js
@@ -1,15 +1,46 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const dbName = 'exampleDb';
 
+// Use environment variable for MongoDB connection
+const url = process.env.MONGODB_URI;  
+const dbName = 'exampleDb';
 let db;
 
 async function getDB() {
-    if (db) return db; 
-    const client = await new MongoClient(url).connect();
-    console.log("Connected to MongoDB");
-    db = client.db(dbName);
-    return db;
+    if (db) return db;
+
+    try {
+        const client = await new MongoClient(url).connect();
+        console.log("Connected to MongoDB");
+        db = client.db(dbName);
+        return db;
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        throw err;
+    }
 }
 
 module.exports = getDB;
+
+
+
+
+
+
+
+
+
+// const { MongoClient } = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'exampleDb';
+
+// let db;
+
+// async function getDB() {
+//     if (db) return db; 
+//     const client = await new MongoClient(url).connect();
+//     console.log("Connected to MongoDB");
+//     db = client.db(dbName);
+//     return db;
+// }
+
+// module.exports = getDB;
